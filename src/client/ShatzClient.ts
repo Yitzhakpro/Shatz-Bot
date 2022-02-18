001d5f4c0c4f86bde5cfcb0c4b532cd2a2677b38
Yitzhak <isako14@gmail.com>
2022-02-18 22:09:19 +0200

    added basic client with load commands support

--- src/client/ShatzClient.ts
+++ src/client/ShatzClient.ts
@@ -1,13 +1,61 @@
 import { Client, Collection } from "discord.js";
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
+import { globPromise } from "../utils";
+import { Command } from "../types";
 
 export class ShatzBotClient extends Client {
-  commands: Collection<string, any> = new Collection();
+  commands: Collection<string, Command> = new Collection();
 
   constructor() {
     super({ intents: 32767 });
   }
 
-  start() {
+  async registerModules() {
+    try {
+      const commandFilesPaths = await globPromise(
+        `${__dirname}/../commands/**/*{.ts,.js}`
+      );
+
+      const filteredCommandFilesPaths = commandFilesPaths.filter((filePath) => {
+        return filePath.endsWith(".ts") || filePath.endsWith(".js");
+      });
+
+      filteredCommandFilesPaths.forEach((commandFilePath) => {
+        const command: Command = require(commandFilePath).default;
+
+        this.commands.set(command.data.name, command);
+      });
+    } catch (err) {
+      console.log("[ERROR] could not load commands");
+      process.exit(1);
+    }
+
+    this.registerCommands();
+  }
+
+  registerCommands() {
+    const token = process.env.botToken!;
+    const guildId = process.env.guildId!;
+    const appId = process.env.appId!;
+    const rest = new REST({ version: "9" }).setToken(token);
+
+    const commands: any[] = [];
+    this.commands.forEach((command) => {
+      const commandJson = command.data.toJSON();
+      commands.push(commandJson);
+    });
+
+    rest
+      .put(Routes.applicationGuildCommands(appId, guildId), {
+        body: commands,
+      })
+      .then(() => console.log("[INFO] Registered application commands"))
+      .catch(console.error);
+  }
+
+  async start() {
+    await this.registerModules();
     this.login(process.env.botToken);
   }
 }
