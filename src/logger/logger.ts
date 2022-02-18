08e4fd8a69a2ac3b6efce30e49d01bf24ccd724b
Yitzhak <isako14@gmail.com>
2022-02-19 00:02:21 +0200

    added pretty pino if not in prod

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,5 +1,11 @@
-import pino from "pino";
+import pino, { LoggerOptions } from "pino";
 
-const logger = pino();
+const pinoOptions: LoggerOptions = {
+  ...(process.env.enviroment !== "prod"
+    ? { transport: { target: "pino-pretty" } }
+    : {}),
+};
+
+const logger = pino(pinoOptions);
 
 export { logger };
